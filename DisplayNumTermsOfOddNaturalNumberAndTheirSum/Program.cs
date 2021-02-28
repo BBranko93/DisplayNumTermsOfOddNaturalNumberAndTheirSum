35912ddfb4da5ef7c7bdee631c6fb84608cc6101
BBranko93 <bane_b80@outlook.com>
2021-02-27 20:42:27 -0700

    Add project files.

---
+++ DisplayNumTermsOfOddNaturalNumberAndTheirSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DisplayNumTermsOfOddNaturalNumberAndTheirSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, sum=0, j;
+            Console.WriteLine("Input number of terms : ");
+            num = int.Parse(Console.ReadLine());
+            Console.Write("The odd numbers are : ");
+            for (i = 1; i <= num; i++)
+            {
+                j = 2 * i - 1;
+                Console.Write(j + " ");
+                sum = sum + j;
+            }
+            //Console.Write("\n");
+            Console.WriteLine("\nThe sum of odd natural numbers up to " + num + " terms : " + sum);
+        }
+    }
+}
